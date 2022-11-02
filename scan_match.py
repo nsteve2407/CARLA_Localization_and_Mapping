bbaa27e1d95ba26223024f0a42d89972d2b8efaa
nsteve2407 <nstephen@tamu.edu>
2022-11-02 18:18:09 -0500

    Add scan_match

---
+++ scan_match.py
@@ -0,0 +1,14 @@
+import pclpy
+
+
+
+def scan_match(source,target):
+    #Inputs:
+    #Soucre: the pointcloud from the current time step
+    # Target: PointCloud from previous time step
+
+    # Output:
+    # pose estimate in the form [x,y,theta]
+
+
+    return 0
