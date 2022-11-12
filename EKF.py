d2c49db40e94cec1c9a59c39c2394d4c050e14d4
nsteve2407 <nstephen@tamu.edu>
2022-11-12 13:07:40 -0600

    Update state to 6D

--- EKF.py
+++ EKF.py
@@ -4,13 +4,13 @@ import numpy as np
 
 class EKF():
     def __init__(self):
-        self.x = np.array[0.0,0.0,0.0] # 3D state vector [x,y,theta]
-        self.P = np.zeros((3,3),dtype=np.float)
-        self.Q = np.zeros((3,3),dtype=np.float)
-        self.R_lidar = np.zeros((3,3),dtype=np.float)
+        self.x = np.array[0.0,0.0,0.0,0.0,0.0,0.0] # 6D state vector [x,y,theta,x',y',theta']
+        self.P = np.eye((6,6),dtype=np.float)
+        self.Q = np.eye((6,6),dtype=np.float)
+        self.R_lidar = np.eye((3,3),dtype=np.float)
 
 
-    def predict(delta_t,state_estimate_t_minus_1,P_t_minus_1,theta,z_t_observation_vector): 
+    def predict(self,delta_t,state_estimate_t_minus_1,P_t_minus_1,theta,z_t_observation_vector): 
 
         # Inputs
 
@@ -62,7 +62,7 @@ class EKF():
         return 0
 
     
-    def measurement_update_lidar(lidar_measurement):
+    def measurement_update_lidar(self,lidar_measurement):
         # Input:
         # Lidar measurement is the lidar position estimate obtained from Scan Matching
         # Lidar measurement is of the form [x,y,theta]
@@ -77,7 +77,7 @@ class EKF():
 
         return 0
 
-    def measurement_update_gps(gps_measurement):
+    def measurement_update_gps(self,gps_measurement):
         # Input:
         # GPS measurement is the noisy measurement of the vehicle's position  obtained from the simulator
         # GPS measurement is of the form [x,y,theta]
