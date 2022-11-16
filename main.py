110235f4c8223e0f118c116f4a88a4e080a790eb
Stephen Ninan <nstephen@tamu.edu>
2022-11-16 01:41:51 -0600

    Add main file

---
+++ main.py
@@ -0,0 +1,264 @@
+#!/usr/bin/env python
+
+import glob
+import os
+import sys
+import argparse
+import time
+from datetime import datetime
+import random
+import numpy as np
+from matplotlib import cm
+import open3d as o3d
+
+try:
+    sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
+        sys.version_info.major,
+        sys.version_info.minor,
+        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+except IndexError:
+    pass
+
+import carla
+
+#Localization Functions
+from EKF import EKF
+from scan_match import ScanMatch
+
+VIRIDIS = np.array(cm.get_cmap('plasma').colors)
+VID_RANGE = np.linspace(0.0, 1.0, VIRIDIS.shape[0])
+
+
+def lidar_sensor_callback(point_cloud, point_list):
+    # Prepares the lidar data for Scan Matching
+    # PCD for scan matching is stored into global variables
+    data = np.copy(np.frombuffer(point_cloud.raw_data, dtype=np.dtype('f4')))
+    data = np.reshape(data, (int(data.shape[0] / 4), 4))
+
+    # Isolate the intensity and compute a color for it
+    intensity = data[:, -1]
+    intensity_col = 1.0 - np.log(intensity) / np.log(np.exp(-0.004 * 100))
+    int_color = np.c_[
+        np.interp(intensity_col, VID_RANGE, VIRIDIS[:, 0]),
+        np.interp(intensity_col, VID_RANGE, VIRIDIS[:, 1]),
+        np.interp(intensity_col, VID_RANGE, VIRIDIS[:, 2])]
+
+    # Isolate the 3D data
+    points = data[:, :-1]
+
+    # We're negating the y to correclty visualize a world that matches
+    # what we see in Unreal since Open3D uses a right-handed coordinate system
+    points[:, :1] = -points[:, :1]
+
+    # # An example of converting points from sensor to vehicle space if we had
+    # # a carla.Transform variable named "tran":
+    # points = np.append(points, np.ones((points.shape[0], 1)), axis=1)
+    # points = np.dot(tran.get_matrix(), points.T).T
+    # points = points[:, :-1]
+
+    point_list.points = o3d.utility.Vector3dVector(points)
+    point_list.colors = o3d.utility.Vector3dVector(int_color)
+
+
+def generate_lidar_bp(arg, world, blueprint_library, delta):
+    #Generates Carla lidar bluprint with noise
+    lidar_bp = blueprint_library.find('sensor.lidar.ray_cast')
+    lidar_bp.set_attribute('noise_stddev', '0.2')
+
+    lidar_bp.set_attribute('upper_fov', str(arg.upper_fov))
+    lidar_bp.set_attribute('lower_fov', str(arg.lower_fov))
+    lidar_bp.set_attribute('channels', str(arg.channels))
+    lidar_bp.set_attribute('range', str(arg.range))
+    lidar_bp.set_attribute('rotation_frequency', str(1.0 / delta))
+    lidar_bp.set_attribute('points_per_second', str(arg.points_per_second))
+    return lidar_bp
+
+
+
+def main(arg):
+# Main function to interact with simulator
+    estimate = True # Run estimator
+    client = carla.Client(arg.host, arg.port)
+    client.set_timeout(2.0)
+    world = client.get_world()
+
+    try:
+        # Initialize world, vehicle, lidar and Open3D visualizer
+        original_settings = world.get_settings()
+        settings = world.get_settings()
+        traffic_manager = client.get_trafficmanager(8000)
+        traffic_manager.set_synchronous_mode(True)
+
+        delta = 0.05
+
+        settings.fixed_delta_seconds = delta
+        settings.synchronous_mode = True
+        settings.no_rendering_mode = arg.no_rendering
+        world.apply_settings(settings)
+
+        blueprint_library = world.get_blueprint_library()
+        vehicle_bp = blueprint_library.filter(arg.filter)[0]
+        vehicle_transform = random.choice(world.get_map().get_spawn_points())
+        vehicle = world.spawn_actor(vehicle_bp, vehicle_transform)
+        vehicle.set_autopilot(arg.no_autopilot)
+
+        lidar_bp = generate_lidar_bp(arg, world, blueprint_library, delta)
+
+        user_offset = carla.Location(arg.x, arg.y, arg.z)
+        lidar_transform = carla.Transform(carla.Location(x=-0.5, z=1.8) + user_offset)
+
+        lidar = world.spawn_actor(lidar_bp, lidar_transform, attach_to=vehicle)
+
+        point_list = o3d.geometry.PointCloud()
+
+        lidar.listen(lambda data: lidar_sensor_callback(data, point_list))
+
+        vis = o3d.visualization.Visualizer()
+        vis.create_window(
+            window_name='Carla Lidar',
+            width=960,
+            height=540,
+            left=480,
+            top=270)
+        vis.get_render_option().background_color = [0.05, 0.05, 0.05]
+        vis.get_render_option().point_size = 1
+        vis.get_render_option().show_coordinate_frame = True
+
+        ## Initialize EKF and ScanMatch objects
+        estimator = EKF()
+        scan_match = ScanMatch()
+
+        frame = 0
+        dt0 = datetime.now()
+        while True:
+            if estimate:
+                # Predict
+                estimator.predict(delta) #delta set to 0.005 secs
+
+                #Update
+                # Use Lidar and gps on alternate frames
+                if frame%2==0:
+                    # Use lidar
+                    pose_scan_match = scan_match.scan_match_map(point_list,estimator.xp[:2])
+                    estimator.measurement_update_lidar(pose_scan_match)
+                else:
+                    #Use gps
+                    tf = vehicle.get_transform()
+                    location = tf.location
+                    rotation = tf.rotation
+                    estimator.measurement_update_gps([location.x,location.y,rotation.yaw])
+
+
+            # Update Visualizer
+            if frame == 2:
+                vis.add_geometry(point_list)
+            vis.update_geometry(point_list)
+
+            vis.poll_events()
+            vis.update_renderer()
+            time.sleep(0.005)
+            world.tick()
+
+            process_time = datetime.now() - dt0
+            sys.stdout.write('\r' + 'FPS: ' + str(1.0 / process_time.total_seconds()))
+            sys.stdout.flush()
+            dt0 = datetime.now()
+            frame += 1
+
+    finally:
+        world.apply_settings(original_settings)
+        traffic_manager.set_synchronous_mode(False)
+
+        vehicle.destroy()
+        lidar.destroy()
+        vis.destroy_window()
+
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser(
+        description=__doc__)
+    argparser.add_argument(
+        '--host',
+        metavar='H',
+        default='localhost',
+        help='IP of the host CARLA Simulator (default: localhost)')
+    argparser.add_argument(
+        '-p', '--port',
+        metavar='P',
+        default=2000,
+        type=int,
+        help='TCP port of CARLA Simulator (default: 2000)')
+    argparser.add_argument(
+        '--no-rendering',
+        action='store_true',
+        help='use the no-rendering mode which will provide some extra'
+        ' performance but you will lose the articulated objects in the'
+        ' lidar, such as pedestrians')
+    argparser.add_argument(
+        '--semantic',
+        action='store_true',
+        help='use the semantic lidar instead, which provides ground truth'
+        ' information')
+    argparser.add_argument(
+        '--no-noise',
+        action='store_true',
+        help='remove the drop off and noise from the normal (non-semantic) lidar')
+    argparser.add_argument(
+        '--no-autopilot',
+        action='store_false',
+        help='disables the autopilot so the vehicle will remain stopped')
+    argparser.add_argument(
+        '--show-axis',
+        action='store_true',
+        help='show the cartesian coordinates axis')
+    argparser.add_argument(
+        '--filter',
+        metavar='PATTERN',
+        default='model3',
+        help='actor filter (default: "vehicle.*")')
+    argparser.add_argument(
+        '--upper-fov',
+        default=15.0,
+        type=float,
+        help='lidar\'s upper field of view in degrees (default: 15.0)')
+    argparser.add_argument(
+        '--lower-fov',
+        default=-25.0,
+        type=float,
+        help='lidar\'s lower field of view in degrees (default: -25.0)')
+    argparser.add_argument(
+        '--channels',
+        default=64.0,
+        type=float,
+        help='lidar\'s channel count (default: 64)')
+    argparser.add_argument(
+        '--range',
+        default=100.0,
+        type=float,
+        help='lidar\'s maximum range in meters (default: 100.0)')
+    argparser.add_argument(
+        '--points-per-second',
+        default=500000,
+        type=int,
+        help='lidar\'s points per second (default: 500000)')
+    argparser.add_argument(
+        '-x',
+        default=0.0,
+        type=float,
+        help='offset in the sensor position in the X-axis in meters (default: 0.0)')
+    argparser.add_argument(
+        '-y',
+        default=0.0,
+        type=float,
+        help='offset in the sensor position in the Y-axis in meters (default: 0.0)')
+    argparser.add_argument(
+        '-z',
+        default=0.0,
+        type=float,
+        help='offset in the sensor position in the Z-axis in meters (default: 0.0)')
+    args = argparser.parse_args()
+
+    try:
+        main(args)
+    except KeyboardInterrupt:
+        print(' - Exited by user.')
--- scan_match.py
+++ scan_match.py
@@ -1,14 +1,31 @@
-import pclpy
+import open3d
+import pypcd
+
+class ScanMatch :
+    def __init__(self):
+        self.map = None #Load HD Map
+        self.ndt = None
+
+    
+    def scan_match_prev_scan(self,source,target,initial_guess):
+        #Inputs:
+        #Soucre: the pointcloud from the current time step
+        # Target: PointCloud from previous time step
+
+        # Output:
+        # pose estimate in the form [x,y,theta]
 
 
+        return 0
+    
+    def scan_match_map(self,source,initial_guess):
+        #Inputs:
+        #Soucre: the pointcloud from the current time step
+        # Target: self.map cloud
 
-def scan_match(source,target):
-    #Inputs:
-    #Soucre: the pointcloud from the current time step
-    # Target: PointCloud from previous time step
-
-    # Output:
-    # pose estimate in the form [x,y,theta]
+        # Output:
+        # pose estimate in the form [x,y,theta]
 
 
-    return 0
+        return 0
+
